7f5c27ace4a69344d28978d6ce5c9f24e28826d7
h1-the-swan <jason.portenoy@gmail.com>
2024-12-19 12:32:31 -0500

    start entity relationship diagram

--- europepmc/README.md
+++ europepmc/README.md
@@ -5,3 +5,32 @@
   + If you are using an ftp client you can use this address to download all of them: <ftp://ftp.ebi.ac.uk>
   + It says that these are updated weekly, though I would want to verify that.
   + There are 52 data files in this folder, organized by repository.
+
+## Data model and entity relationships
+
+```mermaid
+erDiagram
+    CORPUS["Corpus Citation"] {
+        string id PK
+        string publication_id "Currently only DOI allowed"
+        string dataset_id "DOI or accession number"
+        category repository "not necessarily matched to the EuropePMC repository"
+    }
+    EUROPEPMC["EuropePMC Accession Number Citation"] {
+        string accession_number PK
+        string PMCID PK
+        category repository_europepmc
+    }
+    DOI_PMC["EuropePMC to DOI"] {
+        string PMCID PK, FK
+        string DOI PK, FK
+    }
+    REPOSITORY["EuropePMC Repository"] {
+        category repository_europepmc PK, FK
+    }
+    CORPUS one to zero or one DOI_PMC : matches
+    EUROPEPMC one to zero or one DOI_PMC : "identified by"
+    EUROPEPMC one to one REPOSITORY : "in"
+```
+
+Relationships are drawn using <https://en.wikipedia.org/wiki/Entity%E2%80%93relationship_model#Crow's_foot_notation>
--- europepmc/download_data.py
+++ europepmc/download_data.py
@@ -11,17 +11,31 @@ import io
 import pandas as pd
 import numpy as np
 
+import logging
+
+logger = logging.getLogger().getChild(__name__)
+
 URL_ACCESSION_DATA = "https://europepmc.org/ftp/TextMinedTerms"
 
 
 @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_time=60)
 def make_request(url: str, method="GET", **kwargs) -> requests.Request:
-    return requests.request(method=method, url=url, **kwargs)
+    r = requests.request(method=method, url=url, **kwargs)
+    r.raise_for_status()
+    return r
 
 
-def yield_accession_data(
-    url=URL_ACCESSION_DATA, debug=False
-) -> Generator[pd.DataFrame, None, None]:
+def accession_csv_to_dataframe(
+    filepath_or_buffer, filename_or_repository_name: str
+) -> pd.DataFrame:
+    df = pd.read_csv(filepath_or_buffer, dtype="string")
+    colname = filename_or_repository_name.replace(".csv", "")
+    df["repository_europepmc"] = colname
+    df = df.rename(columns={colname: "accession_number"})
+    return df
+
+
+def yield_accession_data(url=URL_ACCESSION_DATA) -> Generator[pd.DataFrame, None, None]:
     r = make_request(url)
 
     # Parse the HTML
@@ -31,30 +45,40 @@ def yield_accession_data(
     csv_files = [
         a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".csv")
     ]
-    # print(f"{len(csv_files)} csv files found")
+    logger.debug(f"{len(csv_files)} csv files found")
 
     for filename in csv_files:
         csv_url = f"{url}/{filename}"
-        print(csv_url)
         r = make_request(csv_url)
-        # r.raise_for_status()
         if "text/csv" not in r.headers.get("content-type", default="").lower():
-            if debug is True:
-                yield csv_url, r
-            else:
-                print(f"skipping {csv_url} because invalid content-type header")
+            logger.error(f"skipping {csv_url} because invalid content-type header")
             continue
-        this_df = pd.read_csv(io.StringIO(r.text), dtype="string")
-        colname = filename.replace(".csv", "")
-        this_df["repository_europepmc"] = colname
-        this_df = this_df.rename(columns={colname: "accession_number"})
+        this_df = accession_csv_to_dataframe(io.StringIO(r.text), filename)
         yield this_df
 
 
-def load_accession_data(url=URL_ACCESSION_DATA) -> pd.DataFrame:
+def prepare_concatenated_dataframe(dfs: Iterable[pd.DataFrame]) -> pd.DataFrame:
+    df = pd.concat(dfs).reset_index(drop=True)
+    df["repository_europepmc"] = df["repository_europepmc"].astype("category")
+    return df
+
+
+def postprocess_europepmc_accession_data(df_acc: pd.DataFrame) -> pd.DataFrame:
+    logger.debug("dropping duplicate rows...")
+    df_acc = df_acc.drop_duplicates(subset=["PMCID", "accession_number"])
+    logger.debug(
+        f"EuropePMC data has {len(df_acc)} rows. {df_acc['PMCID'].isna().sum()} are missing PMCID. Dropping these..."
+    )
+    df_acc = df_acc.dropna(subset=["PMCID"])
+    logger.debug(f"New row count: {len(df_acc)}")
+    return df_acc
+
+
+def load_accession_data(url=URL_ACCESSION_DATA, postprocess: bool = True) -> pd.DataFrame:
     _dfs = []
     for _df in yield_accession_data(url=url):
         _dfs.append(_df)
-    df_acc = pd.concat(_dfs).reset_index(drop=True)
-    df_acc["repository_europepmc"] = df_acc["repository_europepmc"].astype("category")
+    df_acc = prepare_concatenated_dataframe(_dfs)
+    if postprocess is True:
+        df_acc = postprocess_europepmc_accession_data(df_acc)
     return df_acc
